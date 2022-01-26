d49af20ed62df82daabef4b4c4464d54936e575f
EC2 Default User <ec2-user@ip-172-31-87-105.ec2.internal>
2022-01-26 21:43:10 +0000

    lec2

---
+++ lec2.py
@@ -0,0 +1,24 @@
+"""
+this is a regional comment
+"""
+# print ("hello world")
+# print (type(123))
+# print ( 123 )
+# print ( 123.)
+# print ( '123')
+# print (" It's our third official class of IA 241")
+# """
+# do not include this at all
+# """
+# print ("hello world".upper())
+# print ("     hello world     ")
+# print ("     hello world     ".strip())
+# print ("Hello World".upper())
+# print ("Hello World".split())
+# print ("hello , world".split("o"))
+print (2. ** 3)
+#"no" = "yes"
+print ("no")
+#("_no_") = ("yes")
+no = 'yes'
+print ('no')
